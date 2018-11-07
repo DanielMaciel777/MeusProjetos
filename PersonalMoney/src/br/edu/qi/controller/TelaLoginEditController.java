818cf0e2fecf2058b83142282ede1794c541aab8
danie <danie@192.168.1.13>
2018-11-06 22:01:18 -0200

    Incluido a camada de controle

--- PersonalMoney/src/br/edu/qi/controller/TelaLoginEditController.java
+++ PersonalMoney/src/br/edu/qi/controller/TelaLoginEditController.java
@@ -7,15 +7,32 @@ package br.edu.qi.controller;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
  *
- * @author danie
+ * @author Daniel de Abreu Maciel
+ * @version 1.0
  */
 public class TelaLoginEditController implements Initializable {
-
+    @FXML
+    private TextField txUsuario;
+    @FXML
+    private TextField txSenha;
+    @FXML
+    private Button btEntrar;
+    @FXML
+    private Button btLimpar;
+    @FXML
+    private Button btSenha;
+    
+    private Stage dialogStage;
     /**
      * Initializes the controller class.
      */
@@ -24,4 +41,18 @@ public class TelaLoginEditController implements Initializable {
         // TODO
     }    
     
+    @FXML
+    private void handBtEntrar(ActionEvent event){
+        
+    }
+    
+    @FXML
+    private void handBtLimpar(ActionEvent event) {
+        
+    }
+    
+    @FXML
+    private void handBtSenha(ActionEvent event){
+        
+    }
 }
