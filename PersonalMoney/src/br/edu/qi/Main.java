f9dcfafe7d5d2ed5c5afd82f56fa461fed3b7766
danie <danie@192.168.1.13>
2018-11-07 22:33:06 -0200

    Hibernate finalizado + genericDao

--- PersonalMoney/src/br/edu/qi/Main.java
+++ PersonalMoney/src/br/edu/qi/Main.java
@@ -5,12 +5,17 @@
  */
 package br.edu.qi;
 
+import br.edu.qi.controller.TelaLoginEditController;
+import java.io.IOException;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.StackPane;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 
 /**
@@ -20,25 +25,18 @@ import javafx.stage.Stage;
 public class Main extends Application {
     
     @Override
-    public void start(Stage primaryStage) {
-        Button btn = new Button();
-        btn.setText("Say 'Hello World'");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
-            
-            @Override
-            public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
-            }
-        });
-        
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
-        
-        Scene scene = new Scene(root, 300, 250);
-        
-        primaryStage.setTitle("Hello World!");
-        primaryStage.setScene(scene);
-        primaryStage.show();
+    public void start(Stage primaryStage) throws IOException {
+        FXMLLoader loader= new FXMLLoader(Main.class.getResource("view/TelaLoginEdit.fxml"));
+        AnchorPane root= (AnchorPane) loader.load();
+        Stage dialogStage= new Stage();
+        dialogStage.setTitle("Concessionaria");
+        dialogStage.initModality(Modality.WINDOW_MODAL);
+        Scene scene= new Scene(root);
+        dialogStage.setScene(scene);
+        TelaLoginEditController controller= loader.getController();
+        controller.setDialogStage(dialogStage);
+        dialogStage.showAndWait();
+
     }
 
     /**
